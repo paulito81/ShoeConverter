dc1bd5d5f50d90e0f916b09bfc7982a51bb1c7a4
paulit81 <haspau03@student.westerdals.no>
2016-05-03 08:01:28 +0200

    first commit

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //men
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var convertedLabel: UILabel!
+    
+    //woman
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+   
+    @IBOutlet weak var womanConvertedLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+     
+        let sizeFromTextField = Int(mensShoeSizeTextField.text!)
+        let conversionConstant = 30
+        convertedLabel.hidden = false
+        convertedLabel.text = "\(sizeFromTextField! + conversionConstant)" + " in European shoe size"
+        mensShoeSizeTextField.isFirstResponder()
+        mensShoeSizeTextField.text = ""
+    
+    }
+    
+    @IBAction func convertedButtonTwo(sender: UIButton) {
+        
+        let sizeFromTextField = Double((womensShoeSizeTextField.text! as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womanConvertedLabel.hidden = false
+        womanConvertedLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European shoe size"
+        womensShoeSizeTextField.isFirstResponder()
+        womensShoeSizeTextField.text = ""
+    }
+    
 
 }
 
